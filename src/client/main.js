f03cba4d7b387ce600d03c6d687303b90cc985e9
sunluyu <13023509833@163.com>
2026-02-25 21:20:53 +0800

    build: 将 sly-pkg 从本地引用改为 npm 依赖
    添加 sly-pkg 作为项目依赖并更新相关配置

--- src/client/main.js
+++ src/client/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { createSlyUi } from '../../dist/sly-pkg.es.js'
+import { createSlyUi } from 'sly-pkg'
 
 createApp(App).use(createSlyUi()).mount('#app')
